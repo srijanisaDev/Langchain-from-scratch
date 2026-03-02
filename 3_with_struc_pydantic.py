cf12821b712ba78123574f969859a0ee47cbcb35
srijanisaDev <srijankumar770@gmail.com>
2026-03-03 01:51:13 +0530

    json schema

--- 3_with_struc_pydantic.py
+++ 3_with_struc_pydantic.py
@@ -1,2 +1,63 @@
 ## pydantic is a data validation and data parsing library for python . It measures that the data you work with is 
 ## correct , structured , and type- safe
+
+from dotenv import load_dotenv
+from typing import TypedDict, Annotated, Optional, Literal
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from pydantic import Field , BaseModel
+
+load_dotenv()
+
+
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0,
+)
+
+
+class Review(TypedDict):
+    key_themes : list[str] = Field(description="Write down all the key themes discussed in the review in a list")
+    summary : str = Field(description="A brief summary of the review")
+    sentiment: Literal["pos" , "neg"] = Field(description="Return sentiment of the review either negative , positive or neutral")
+    pros: Optional[list[str]] = Field(default=None , description="Write down all the pros inside a list")
+    cons: Optional[list[str]] = Field(default=None , description="Write down all the cons inside a list")
+    name : Optional[str] = Field(default=None , description="Write down the name of the reviewer")
+
+
+   
+
+
+
+structured_model = model.with_structured_output(Review)
+
+result = structured_model.invoke(""" I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+
+Cons:
+Bulky and heavy—not great for one-handed use
+Bloatware still exists in One UI
+Expensive compared to competitors
+                                 
+
+Review by Srijan                                 
+
+
+
+
+""")
+    
+
+print(result["name"])
+print(result["sentiment"])
+print(result["key_themes"])
